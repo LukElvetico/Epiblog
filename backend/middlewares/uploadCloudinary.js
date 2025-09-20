57659d6ae9f0f40d2c9715e08d501b0d17e9f96a
LukElvetico <Weber.ltm@gmail.com>
2025-09-21 00:18:41 +0200

    Update uploadCloudinary.js

--- backend/middlewares/uploadCloudinary.js
+++ backend/middlewares/uploadCloudinary.js
@@ -3,7 +3,7 @@ import { v2 as cloudinary } from 'cloudinary';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
 
 // configurazione spostata fuori per poter utilizzare cloudinary anche senza multer (quando eliminiamo i file)
-/*cloudinary.config({
+/* cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
@@ -23,3 +23,4 @@ const uploadCloudinary = multer({ storage: storageCloudinary });
 
 // export default uploadCloudinary;
 export { cloudinary, uploadCloudinary };
+*/
