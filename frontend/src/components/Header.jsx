6ea966a269055f2b8c00242da72daba2bb6da575
LukElvetico <Weber.ltm@gmail.com>
2025-09-21 01:20:20 +0200

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -49,7 +49,7 @@ function Header({ isAuthenticated, onLogout }) {
     };
 
     return (
-        <Navbar bg="bg-primary" expand="lg" className="mb-4">
+        <Navbar bg="dark" expand="lg" className="mb-4">
             <Container>
                 <Navbar.Brand as={Link} to="/">EpiBlog</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
