ff9ca0c316b25f31e0ff7afb1b5c6459e5f1a48d
LukElvetico <Weber.ltm@gmail.com>
2025-09-21 00:35:18 +0200

    Update auth.router.js

--- backend/routers/auth.router.js
+++ backend/routers/auth.router.js
@@ -11,18 +11,14 @@ const router = Router();
 router.post('/register', async (request, response, next) => {
     try {
         const body = request.body;
-
-        // Verifica se l'utente esiste già
         const user = await User.findOne({ email: body.email.toLowerCase() });
         if (user) {
             return next(createHttpError.Conflict('Email già usata'));
         }
 
-        // Hasha la password
         const hash = await bcrypt.hash(body.password, 10);
         const insertedUser = await User.create({ ...body, password: hash });
-
-        // Invia l'email di benvenuto
+        
         const htmlContent = `
             Benvenuto a bordo, divertiti a cucinare
             <br>
@@ -40,46 +36,34 @@ router.post('/register', async (request, response, next) => {
             html: htmlContent,
         });
 
-        // Invia la risposta SOLO dopo che tutte le operazioni sono state completate con successo
-        // MESSAGGIO DI CORRETTA REGISTRAZIONE AGGIUNTO QUI 👇
         response.send({ message: 'Registrazione effettuata correttamente, procedi con l\'accesso' });
 
     } catch (error) {
-        // Se c'è un errore (es. fallimento dell'invio email), passa l'errore al middleware
-        next(error);
+              next(error);
     }
 });
 
 router.post('/verify', async (request, response, next) => {
-    // Qui devi decodificare il token, cercare l'utente nel DB
-    // e aggiornare lo stato di "verificato"
 });
 
 router.post('/login', async (request, response, next) => {
     const body = request.body;
-
-    // Cerchiamo l'utente con la mail
     const user = await User.findOne({ email: body.email.toLowerCase() }).select(
         '+password'
     );
-
-    // Se non lo troviamo -> errore
     if (!user) {
         return next(createHttpError.Unauthorized('Credenziali sbagliate'));
     }
 
-    // Verifichiamo la password
     if (!(await bcrypt.compare(body.password, user.password))) {
         return next(createHttpError.Unauthorized('Credenziali sbagliate'));
     }
 
-    // Genera il token JWT con scadenza di 1 ora
     const token = await generateJWT(
         { userId: user._id },
-        '1h' // Passa il tempo di scadenza desiderato
+        '1h' 
     );
 
-    // Invia il token e i dati dell'utente per aggiornare il frontend
     response.send({ token, user: user.toObject({ getters: true }) });
 });
 
